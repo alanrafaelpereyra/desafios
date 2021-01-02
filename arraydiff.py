55ffa61c0a34496045ebc03f545a5f8741c292a1
alanrafaelpereyra <alanpereyra08@gmail.com>
2021-01-02 17:59:10 -0300

    encontrando_diferencias entre listas

---
+++ arraydiff.py
@@ -0,0 +1,20 @@
+# Your goal in this kata is to implement a difference function,
+#  which subtracts one list from another and returns the result.
+# It should remove all values from list a, which are present in list b.
+
+# Test.describe("Basic Tests")
+# Test.assert_equals(array_diff([1,2], [1]), [2], "a was [1,2], b was [1], expected [2]")
+# Test.assert_equals(array_diff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1], expected [2,2]")
+# Test.assert_equals(array_diff([1,2,2], [2]), [1], "a was [1,2,2], b was [2], expected [1]")
+# Test.assert_equals(array_diff([1,2,2], []), [1,2,2], "a was [1,2,2], b was [], expected [1,2,2]")
+# Test.assert_equals(array_diff([], [1,2]), [], "a was [], b was [1,2], expected []")
+
+
+def array_diff(a, b):
+    result = []
+
+    for i in a:
+        if i not in b:
+            result.append(i)
+            
+    return result
